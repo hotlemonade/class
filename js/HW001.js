12d3f32456e6590e5611ab231be792708ac0e09b
hotlemonade <dohs0131@gmail.com>
2024-01-11 17:44:58 +0900

    add

---
+++ js/HW001.js
@@ -0,0 +1,12 @@
+let animals = ['dog', 'cat', 'cow']
+
+for(let i=0; i < animals.length; i++){
+    console.log(animals[i])
+}
+
+let numbers = [1,3,5,2,8,11]
+let result = numbers.filter( ( num ) => num < 10 );
+console.log(result)
+
+
+
