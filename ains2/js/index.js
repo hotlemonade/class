190e121fa5acf0832f39c85dcf161a5149a8ea2f
hotlemonade <dohs0131@gmail.com>
2024-02-25 21:21:31 +0900

    add

---
+++ ains2/js/index.js
@@ -0,0 +1,30 @@
+// btn 을 눌렀을때 opne class 가 붙고 다시 눌렀을때 class를 때줘라
+
+
+ 
+
+function openMenu(){
+
+    const ham = document.querySelector('.btn-ham');
+    const modal = document.querySelector('.menu-modal')
+    
+    ham.addEventListener('click', (e) => {
+        console.log(`e.target : ${e.target}`)
+        console.log(`e.currentTarget : ${e.currentTarget}`)
+
+        let target = e.currentTarget;
+        target.classList.toggle('open');
+        if(target.classList.contains('open')) {
+            modal.classList.add('open')
+        } else {
+            modal.classList.remove('open')
+        }
+        
+    })
+ 
+
+}
+
+openMenu()
+
+
---
+++ ains2/js/test.js
@@ -0,0 +1,7 @@
+let arr = [1,2,3,4,5];
+let num1 = 1;
+let num2 = 3;
+
+
+let result = arr.slice(num1, num2 + 1)
+console.log(result)
